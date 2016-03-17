008a7c7beedc7a4cffcbac62b5e4c0d7f726d3d8
Jeff Wintersinger <jeff.git@wintersinger.com>
2016-03-17 10:23:23 -0400

    Permit CNV directory to be specified as argument.

--- index_data.py
+++ index_data.py
@@ -3,26 +3,31 @@ import os
 import glob
 import json
 import os
+import sys
 from collections import defaultdict
 
 def main():
   datasets = {}
 
-  base_dir = 'data'
-  for cnv_cmp in glob.glob(os.path.join(base_dir, '*.json')):
-    with open(cnv_cmp) as cnvf:
-      cn_calls = json.load(cnvf)
+  if len(sys.argv) > 1:
+    base_dir = os.path.realpath(sys.argv[1])
+  else:
+    base_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 
+  for cnv_cmp in glob.glob(os.path.join(base_dir, '*.json')):
     dataset_name = cnv_cmp.split('/')[-1].split('.')[0]
     if dataset_name in ('index', 'metadata'):
       continue
+    with open(cnv_cmp) as cnvf:
+      cn_calls = json.load(cnvf)
+
     datasets[dataset_name] = {
-      'cn_calls_path': cnv_cmp,
+      'cn_calls_path': 'data/%s.json' % dataset_name,
       'methods': cn_calls['methods'],
       'genome_proportions': cn_calls['genome_proportions']
     }
 
-  out_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'index.json')
+  out_path = os.path.join(base_dir, 'index.json')
   with open(out_path, 'w') as outf:
     print(json.dumps(datasets), file=outf)
 
